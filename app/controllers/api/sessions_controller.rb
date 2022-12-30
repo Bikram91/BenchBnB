86dd15faec4a3393d4fa8c13d527d94468a9e9ae
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-12-30 00:02:22 -0800

    first commit

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::SessionsController < ApplicationController
+  def show
+      
+    if current_user
+      @user = current_user
+      # render json: @user
+      # render :show #2
+      render 'api/users/show'
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:credential], params[:password])
+    if @user
+      login!(@user)
+      # render json: @user
+      # render :show #3
+      render 'api/users/show'
+    else
+      render json: { errors: ["The provided credentials were invalid."] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    logout!
+    render json: { message: "success" }
+  end
+end
---
+++ frontend/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
+import * as sessionActions from "./store/session";
+
+
+
+const store = configureStore();
+
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
+}
+
+
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+// if (sessionStorage.getItem("X-CSRF-Token") === null) {
+//   restoreCSRF().then(renderApplication);
+// } else {
+//   renderApplication();
+// }
+
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null 
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}
+
+// import {csrfFetch} from "../store/csrf"
+
+// // Action Type
+// const LOGIN_USER = "users/lOGIN_USER"
+// const LOGOUT_USER = "users/LOGOUT_USER"
+// // Action Creators
+// export const receiveUser = user => ({
+//     type: LOGIN_USER,
+//     payload: user
+
+
